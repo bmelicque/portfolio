d1212d4826e62d0c63c4078e79ade61bcc3831a4
bmelicque <bmelicque@gmail.com>
2022-02-16 14:52:54 +0100

    Languages and frameworks

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,17 +1,29 @@
 import { Heading, SubHeading } from "./headings";
 import { Skill, LANGUAGES } from "../lib/data/skills";
+import Image from "next/image";
+
+const ICON_PATH_ROOT = "/assets/logos/";
+const ICON_SIZE = 24;
 
 // Skill Item
 function Skill({ icon, name, value }: Skill) {
 	return (
 		<li className="grid grid-cols-12 items-center">
-			<span className="col-span-4">
-				{!!icon && <i>I</i>}
+			<span className="col-span-5 flex gap-2">
+				{!!icon && (
+					<Image
+						src={ICON_PATH_ROOT + icon}
+						width={ICON_SIZE}
+						height={ICON_SIZE}
+						objectFit="contain"
+						alt=""
+					/>
+				)}
 				{name}
 			</span>
-			<span className="col-span-8 relative inline-block bg-gray-300 h-2 rounded">
+			<span className="col-span-7 relative inline-block bg-gray-300 h-2 rounded overflow-hidden">
 				<span
-					className="absolute inline-block bg-primary h-2 w-full rounded origin-left"
+					className="absolute inline-block bg-primary h-2 w-full rounded-lg origin-left"
 					style={{ transform: `scaleX(${value / 100})` }}
 				></span>
 			</span>
@@ -29,7 +41,7 @@ function SkillList({ category, skills }: SkillList) {
 	return (
 		<section>
 			<SubHeading>{category}</SubHeading>
-			<ul>
+			<ul className="flex flex-col gap-3	">
 				{skills.map(({ icon, name, value }) => (
 					<Skill key={name} icon={icon} name={name} value={value} />
 				))}
@@ -44,7 +56,7 @@ export default function Profile() {
 		<section className="container mx-auto py-16 px-0.5">
 			<Heading>Mes compétences</Heading>
 
-			<div id="skills-container" className="grid grid-cols-3">
+			<div id="skills-container" className="grid grid-cols-3 gap-24">
 				<SkillList category="Langages et frameworks" skills={LANGUAGES} />
 			</div>
 		</section>
--- lib/data/skills.ts
+++ lib/data/skills.ts
@@ -6,27 +6,68 @@ export interface Skill {
 
 export const LANGUAGES: Skill[] = [
 	{
+		icon: "javascript.svg",
 		name: "Javascript",
-		value: 80,
+		value: 90,
 	},
 	{
+		icon: "typescript.svg",
 		name: "Typescript",
-		value: 40,
+		value: 50,
 	},
 	{
+		icon: "react.png",
+		name: "React.js",
+		value: 65,
+	},
+	{
+		icon: "next.png",
+		name: "Next.js",
+		value: 65,
+	},
+	{
+		icon: "vue.png",
+		name: "Vue.js",
+		value: 10,
+	},
+	{
+		icon: "node.png",
+		name: "Node.js",
+		value: 60,
+	},
+	{
+		icon: "express.png",
+		name: "Express.js",
+		value: 60,
+	},
+	{
+		icon: "php.png",
 		name: "PHP",
+		value: 10,
+	},
+	{
+		icon: "html.png",
+		name: "HTML",
+		value: 90,
+	},
+	{
+		icon: "css.png",
+		name: "CSS",
+		value: 90,
+	},
+	{
+		icon: "sass.png",
+		name: "SaSS",
+		value: 60,
+	},
+	{
+		icon: "bootstrap.png",
+		name: "Bootstrap",
 		value: 20,
 	},
 	{
-		name: "HTML",
-		value: 80,
-	},
-	{
-		name: "CSS",
-		value: 80,
-	},
-	{
-		name: "SaSS",
-		value: 60,
+		icon: "tailwind.png",
+		name: "Tailwind CSS",
+		value: 65,
 	},
 ];
